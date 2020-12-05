a8f03685ab12ac6fc90ef1c48dfe2cd9f32c2967
herypank <60302361+herypank@users.noreply.github.com>
2020-12-05 19:19:27 +0300

    Добавил dialogflow

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 import os
 import logging
 
+
+import dialogflow_v2 as dialogflow
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import (
+    Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    level=logging.INFO
+)
 
 logger = logging.getLogger(__name__)
 
 
-def start(bot, update):
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    text_input = dialogflow.types.TextInput(
+        text=texts, language_code=language_code)
+
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
+    
+    return response.query_result.fulfillment_text
+
+
+def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     update.message.reply_text('Здравствуйте!')
 
 
-def help(bot, update):
+def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
-def echo(bot, update):
+def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def error(bot, update, error):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
+    project_id = os.getenv("PROJECT_ID")
+    language_code = os.getenv("LANGUAGE_CODE")
+    session_id = update.effective_user
+    texts = update.message.text
+    
+    update.message.reply_text(
+        detect_intent_texts(project_id, session_id, texts, language_code))
 
 
 def main():
@@ -37,18 +64,18 @@ def main():
     load_dotenv()
     updater = Updater(token=os.getenv("TG_TOKEN"), use_context=True)
 
-    dp = updater.dispatcher
+    dispatcher = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(
+        Filters.text & ~Filters.command, echo))
 
-    dp.add_error_handler(error)
 
     updater.start_polling()
 
-    updater.idle()
+    # updater.idle()
 
 
 if __name__ == '__main__':
