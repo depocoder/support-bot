35bc3b1522d74362667bdb0d3a2c0474d6f9a0e8
herypank <60302361+herypank@users.noreply.github.com>
2020-12-05 22:55:57 +0300

    Добавил dialogflow в vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,17 +1,22 @@
 import os
 import random
-
+from tg_bot import detect_intent_texts
 from dotenv import load_dotenv
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def echo(event, vk_api):
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1, 1000)
-    )
+def reply_from_dialogflow(event, vk_api):
+    project_id = os.getenv("PROJECT_ID")
+    language_code = os.getenv("LANGUAGE_CODE")
+    response = detect_intent_texts(
+        project_id, event.user_id, event.text, language_code)
+    if not response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":
@@ -22,5 +27,4 @@ if __name__ == "__main__":
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
-
+            reply_from_dialogflow(event, vk_api)
