96baceceaf77a0947a15ab528d5b538e7f3f722f
herypank <60302361+herypank@users.noreply.github.com>
2020-12-07 12:59:45 +0300

    Удалил лишний код

--- vk_bot.py
+++ vk_bot.py
@@ -9,11 +9,6 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
 logger = logging.getLogger(__name__)
 
 
