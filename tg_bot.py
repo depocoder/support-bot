7c91f384800662a49829839cd1b82c89e23bb0a6
herypank <60302361+herypank@users.noreply.github.com>
2020-12-07 18:56:47 +0300

    Высушил код

--- tg_bot.py
+++ tg_bot.py
@@ -39,15 +39,6 @@ def error_handler(update: Update, context: CallbackContext) -> None:
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
     tb_string = ''.join(tb_list)
 
-    message = (
-        f'An exception was raised while handling an update\n'
-        f'<pre>update = {html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False))}'
-        '</pre>\n\n'
-        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
-        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
-        f'<pre>{html.escape(tb_string)}</pre>'
-    )
-
     context.bot.send_message(chat_id=os.getenv("DEVELOPER_CHAT_ID"), text=message, parse_mode=ParseMode.HTML)
 
 
