ce13aaba27090e223dd44edca95ee6cf6e357b02
herypank <60302361+herypank@users.noreply.github.com>
2020-12-07 12:59:37 +0300

    Добавил логирование ошибок в телеграм боте

--- tg_bot.py
+++ tg_bot.py
@@ -1,16 +1,15 @@
 import os
 import logging
+import traceback
+import html
+import json
 from detect_intent_texts import detect_intent_texts
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import Update, ParseMode
 from telegram.ext import (
     Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 logger = logging.getLogger(__name__)
 
 
@@ -34,11 +33,30 @@ def reply_from_dialogflow(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(response.query_result.fulfillment_text)
 
 
+def error_handler(update: Update, context: CallbackContext) -> None:
+    logger.error(
+        msg="Exception while handling an update:", exc_info=context.error)
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    context.bot.send_message(chat_id=os.getenv("DEVELOPER_CHAT_ID"), text=message, parse_mode=ParseMode.HTML)
+
+
 def main():
     """Start the bot."""
     load_dotenv()
     updater = Updater(token=os.getenv("TG_TOKEN"), use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.add_error_handler(error_handler)
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
